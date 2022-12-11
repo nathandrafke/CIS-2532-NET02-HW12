743b9b9f0c1a4976c2d6b3cfaaa6ab97e4ccdee1
nathandrafke <119820901+nathandrafke@users.noreply.github.com>
2022-12-11 14:06:18 -0600

    Delete hw12.py

--- hw12.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('books.db')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM titles")
-description = cursor.description
-data = cursor.fetchall()
-for row in data:
-    print("%-20s%-20s%-20s" % (row[0], row[1], row[2]))
